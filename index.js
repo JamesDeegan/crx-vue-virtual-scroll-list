0b7b4d3b45b36682eea6c8799dc83ca85384f3a8
JamesDeegan <jamesdeegan@hotmail.co.za>
2018-11-20 13:37:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -293,7 +293,7 @@
             // filter the shown items base on `start` and `end`.
             filter: function () {
                 var delta = this.delta
-                var slots = this.$slots.draggable[0].elm.children
+                var slots = this.$slots.draggable[0]
                 console.log('------slots----- 297')
                 console.log(slots)
 
