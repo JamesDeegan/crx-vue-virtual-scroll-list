1cdb847c8aef7180e2e74d816aa07da481ac27bc
JamesDeegan <jamesdeegan@hotmail.co.za>
2018-11-20 13:51:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -293,9 +293,11 @@
             // filter the shown items base on `start` and `end`.
             filter: function () {
                 var delta = this.delta
-                var slots = this.$slots.default.elm
+                var slots = this.$slots.default
+                console.log('-- DEFAULT SLOT --')
+                console.log(this.$slots.default)
                 console.log('------slots----- 297')
-                console.log(slots)
+                console.log(this.$slots.default[0])
 
                 if (!slots) {
                     slots = []
